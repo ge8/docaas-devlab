4db58d40bd8b0848c7fe715332f86603dfff1395
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2019-01-31 16:43:46 +1100

    test

--- backend/deploy-master.sh
+++ backend/deploy-master.sh
@@ -4,6 +4,8 @@ export BUCKET=docaas-summit
 export DOMAIN=estaba.net
 export REGION=ap-southeast-2
 
+aws s3 cp cognito.yaml docaas-summit
+
 sam package --template-file master-template.yaml --s3-bucket $BUCKET --output-template-file packaged.yaml --region $REGION
 sam deploy --template-file packaged.yaml --stack-name docaas-summit --capabilities CAPABILITY_NAMED_IAM --region $REGION
 

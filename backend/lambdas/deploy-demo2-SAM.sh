c2b4aafb5c7707e5993d93799fa6d860de344c34
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2019-02-25 17:10:15 +1100

    Cognito done, SAM failing IdentityPool::Id reference

--- backend/lambdas/deploy-demo2-SAM.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-. ./../demos/loadvariables.sh
-
-sam package --template-file demo2-template.yaml --s3-bucket $BUCKET --output-template-file packaged.yaml --region $REGION
-sam deploy --template-file packaged.yaml --stack-name docaas --capabilities CAPABILITY_NAMED_IAM --region $REGION
-
-rm -f packaged.yaml

f075ff60a5fbb83d622ee49f7aae4c877dab072b
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2019-02-28 16:42:16 +1100

    cf, bucket yet to be tested for hosting done.

--- backend/deploy-cf.sh
+++ backend/deploy-cf.sh
@@ -3,13 +3,13 @@
 export BUCKET=docaas-summit
 # export DOMAIN=estaba.net
 export REGION=ap-southeast-2
-export STACK=docaas
+export STACK=cfn-docaas
 
 # aws s3 cp cognito.yaml s3://docaas-summit
 # aws cloudformation delete-stack --stack-name $STACK
 # aws cloudformation wait stack-delete-complete --stack-name $STACK
 
-sam package --template-file template.yaml --s3-bucket $BUCKET --output-template-file packaged.yaml --region $REGION
+sam package --template-file cf.yaml --s3-bucket $BUCKET --output-template-file packaged.yaml --region $REGION
 sam deploy --template-file packaged.yaml --stack-name $STACK --capabilities CAPABILITY_NAMED_IAM --region $REGION
 
 rm -f packaged.yaml

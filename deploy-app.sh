c38005885422f405e1654b9517a8a66afa29b6b9
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2019-03-17 14:21:29 +1100

    change

--- deploy-app.sh
+++ deploy-app.sh
@@ -4,18 +4,6 @@ export SAMBUCKET=docaas-summit
 export REGION=ap-southeast-2
 export STACK=docaas-summit
 
-
-# Grab bucket name from cloudformation output
-WEBBUCKET=$(aws cloudformation describe-stacks --stack-name $STACK --query 'Stacks[0].Outputs[?OutputKey==`TheBucket`].OutputValue' --output text)
-echo $WEBBUCKET
-
-# Build app
-cd frontend
-npm install
-npm run-script build
-cd ..
-
-
 # Grab DomainName from cloudformation output
 DOMAINNAME=$(aws cloudformation describe-stacks --stack-name $STACK --query 'Stacks[0].Outputs[?OutputKey==`DomainName`].OutputValue' --output text)
 # Configure DomainName in homepage (summit.docaas.net)
@@ -25,9 +13,18 @@ rm -f frontend/package.json-e
 # Grab CloudfrontEndpoint from cloudformation output
 CF=$(aws cloudformation describe-stacks --stack-name $STACK --query 'Stacks[0].Outputs[?OutputKey==`CloudfrontEndpoint`].OutputValue' --output text)
 # Configure CF endpoint in MainBody
-find ./frontend/src/components/MainBody -type f -exec sed '' -i -e "s/ENDPOINT.com/$CF/g" {} \;
-# rm -f frontend/src/components/MainBody/MainBody.js-e frontend/src/components/MainBody/MainBody.css-e 
+find ./frontend/src/components/MainBody -type f -exec sed -i -e "s/ENDPOINT.com/$CF/g" {} \;
+rm -f frontend/src/components/MainBody/MainBody.js-e frontend/src/components/MainBody/MainBody.css-e 
 
+# Build app
+cd frontend
+npm install
+npm run-script build
+cd ..
+
+# Grab bucket name from cloudformation output
+WEBBUCKET=$(aws cloudformation describe-stacks --stack-name $STACK --query 'Stacks[0].Outputs[?OutputKey==`TheBucket`].OutputValue' --output text)
+echo $WEBBUCKET
 
 # Copy front-end public files to bucket
 aws s3 sync frontend/build/ "s3://$WEBBUCKET" --delete

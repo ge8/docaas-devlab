ea922e0e9cb3cb71d909e86738fa9f4b0af88f41
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2019-03-28 10:49:59 +1100

    add APIGW deploy - yet to be tested

--- update-template.sh
+++ update-template.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# Load Variables
 . ./load-variables.sh
 
-# aws cloudformation delete-stack --stack-name $STACK
-# aws cloudformation wait stack-delete-complete --stack-name $STACK
-
+# Package and Deploy SAM Template
 sam package --template-file backend/template.yaml --s3-bucket $SAMBUCKET --output-template-file backend/packaged.yaml --region $REGION
 sam deploy --template-file backend/packaged.yaml --stack-name $STACK --capabilities CAPABILITY_NAMED_IAM --region $REGION
-
 rm -f backend/packaged.yaml
+
+# API Gateway Deploy
+RESTAPI=$(aws cloudformation describe-stacks --stack-name $STACK --query 'Stacks[0].Outputs[?OutputKey==`APIBaseURL`].OutputValue' --output text)
+DEPID=$(aws apigateway create-deployment --rest-api-id $RESTAPI --stage-name Prod --query "id" --output text)
+aws apigateway update-stage --rest-api-id $RESTAPI --stage-name Prod --patch-operations op='replace',path='/deploymentId',value=$DEPID

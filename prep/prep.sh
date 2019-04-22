fdd253ad5aa7f16a70d05b8b5162a4fb7e51da06
Estaba <grestaba@9801a79671c3.ant.amazon.com>
2019-04-22 22:19:11 +1000

    no script for prep-template

--- prep/prep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Deploy prep-template to create R53, S3, output Name Servers.
-sam deploy --stack-name docaas-devlab-prep1 --template-file prep/prep-template.yaml --capabilities CAPABILITY_NAMED_IAM --region $REGION
-
-
-
